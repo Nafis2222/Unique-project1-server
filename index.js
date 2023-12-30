33021c03076627991ab0a4428462a0eafcdead46
Nafis <bhuiyannafis517@gmail.com>
2023-12-30 22:09:23 +0600

    last

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ const port = process.env.PORT || 5000
 // unique-project1
 // IPtaOaxopfb7QunG
 
+app.use(cors())
+app.use(express.json())
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://unique-project1:IPtaOaxopfb7QunG@cluster0.e9gq9mr.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,13 +30,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    const databese = client.db('Unique-project1').collection('post')
+
+    app.post('/post' , async(req,res)=>{
+      const user = req.body
+      const result = await databese.insertOne(user)
+      res.send(result)
+      console.log(result)
+    })
+
+    app.get('/post', async(req,res)=>{
+      const result = await databese.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/post/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await databese.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/post/:id', async(req,res)=>{
+      const id = res.params.id 
+      const data = req.body
+      const filter = { _id: new ObjectId(id) };
+      const result = await databese.updateOne(filter)
+      res.send(result)
+
+    })
+
+
+
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
